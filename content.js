5f5f11034a9bc2ef515c55c86f6376975220d137
Shiyu <shiyuchen6688@gmail.com>
2019-12-20 02:12:50 -0800

    first

---
+++ content.js
@@ -0,0 +1,12 @@
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sneder, sendResponse) {
+    console.log(message.txt);
+    if(message.txt === "hello") {
+        let paragraphs = document.getElementsByTagName('p');
+        for(elt of paragraphs) {
+            elt.innerHTML = "TIME TO STUDY!"
+            elt.style["background-color"] = '#FFFF00';
+        }
+    }
+}
